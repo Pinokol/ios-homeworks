315f6d4ed5e09db5ee517c7f18f77a768b914326
Mishin Vitaliy <grand8329@gmail.com>
2023-08-05 22:58:37 +0300

    2.1. UIView, CALayer, UIWindow. UI-базовые компоненты

---
+++ Navigation/Data/Profile/ProfileHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Виталий Мишин on 29.07.2023.
+//
+
+import Foundation
---
+++ Navigation/Data/Profile/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Виталий Мишин on 29.07.2023.
+//
+
+import Foundation
